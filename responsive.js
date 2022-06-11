93cb0ececc2b1ebc30df58141773a59a51fc40cf
¨jonathansansok¨ <jonasans2@live.com.ar>
2022-06-11 16:19:54 -0300

    js

--- responsive.js
+++ responsive.js
@@ -1,3 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('.cieloLista').classList.remove('show');
+});
 document.querySelector('.menu-btn').addEventListener('click', function() {
     document.querySelector('.cieloLista').classList.toggle('show');
 });
